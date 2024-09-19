983e685a5342819a142a72d06829fb0d7f05249b
tobiajras <tobiajras@hotmail.com>
2024-09-19 11:05:41 -0300

    product details color

--- src/app/productos/[productId]/page.tsx
+++ src/app/productos/[productId]/page.tsx
@@ -1,28 +1,44 @@
 'use client';
 
+import { useState, useEffect } from 'react';
 import products from '@/data/products.json';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
 
 const colorMap: { [key: string]: string } = {
-  Negro: '#000000',
-  Blanco: '#FFFFFF',
-  Rojo: '#FF0000',
-  Azul: '#1f263f',
-  Verde: '#008000',
-  Amarillo: '#FFFF00',
-  Morado: '#800080',
-  Naranja: '#FFA500',
-  Gris: '#808080',
+  Negro: 'bg-[#000000]',
+  Blanco: 'bg-[#FFFFFF]',
+  Rojo: 'bg-[#FF0000]',
+  Azul: 'bg-[#1f263f]',
+  Verde: 'bg-[#008000]',
+  Amarillo: 'bg-[#FFFF00]',
+  Morado: 'bg-[#800080]',
+  Naranja: 'bg-[#FFA500]',
+  Gris: 'bg-[#808080]',
 };
 
 const ProductId = ({ params }) => {
   const [productById, setProductById] = useState(null);
+  const [selectedColor, setSelectedColor] = useState('');
 
   useEffect(() => {
     const product = products.find((product) => product.id == params.productId);
     setProductById(product);
+    // Establecer el primer color como color por defecto si existe
+    if (product && product.color && product.color.length > 0) {
+      setSelectedColor(product.color[0].color);
+    }
   }, [params.productId]);
+
+  // Función para obtener el objeto de color seleccionado
+  const getSelectedColorObject = () => {
+    if (!productById || !productById.color) return null;
+    return productById.color.find((c) => c.color === selectedColor);
+  };
+
+  // Ejemplo de cómo usar el color seleccionado
+  const selectedColorObject = getSelectedColorObject();
+
+  console.log(selectedColor);
 
   return (
     <section className='flex justify-center mx-10 my-20'>
@@ -33,7 +49,7 @@ const ProductId = ({ params }) => {
               <Image
                 className='w-full h-full object-contain object-bottom'
                 src={`/assets/products/${
-                  productById.image_url || productById.color?.[0]?.image_url
+                  selectedColorObject?.image_url || productById.image_url
                 }`}
                 alt={productById.name}
                 width={150}
@@ -43,7 +59,7 @@ const ProductId = ({ params }) => {
           </article>
           <article>
             <div className=''>
-              <h4 className='text-color-primary font-semibold text-3xl h-16 line-clamp-2 mb-1 max-w-96'>
+              <h4 className='text-color-primary font-semibold text-3xl h-20 line-clamp-2 mb-1 max-w-96'>
                 {productById.name}
               </h4>
               <div className='mt-2'>
@@ -52,23 +68,30 @@ const ProductId = ({ params }) => {
                 </span>
               </div>
               <div className='flex flex-col gap-5 mt-5'>
-                <div>
-                  <h6 className=''>Colores</h6>
-                  <div className='flex gap-5 mt-3 ml-1'>
-                    {productById?.color.map((color, idx) => (
-                      <div
-                        className={`w-8 h-8 rounded-full ring-1 ring-offset-4 ring-color-title bg-[${
-                          colorMap[color.color]
-                        }] hover:ring-color-primary hover:ring-2 transition-all cursor-pointer`}
-                        key={idx}
-                      ></div>
-                    ))}
+                {productById.color && (
+                  <div>
+                    <h6 className=''>Colores</h6>
+                    <div className='flex gap-5 mt-3 ml-1'>
+                      {productById.color.map((color, idx) => (
+                        <div
+                          key={idx}
+                          onMouseEnter={() => setSelectedColor(color.color)}
+                          className={`${
+                            colorMap[color.color]
+                          } w-8 h-8 rounded-full ring-1 ring-offset-4 ring-color-title hover:ring-color-primary hover:ring-2 transition-all cursor-pointer ${
+                            selectedColor === color.color
+                              ? 'ring-2 ring-color-primary'
+                              : ''
+                          }`}
+                        ></div>
+                      ))}
+                    </div>
                   </div>
-                </div>
+                )}
               </div>
               <div className='mt-10'>
                 <button className='bg-color-primary hover:bg-color-primary-dark transition-colors text-white px-10 py-3 rounded'>
-                  Añandir al carrito
+                  Añadir al carrito
                 </button>
               </div>
             </div>
