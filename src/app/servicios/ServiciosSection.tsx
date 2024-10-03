829e01ca5c01f4cf9dd3889f64d83193971a712f
tobyajras <tobyajras@gmail.com>
2024-10-03 15:40:02 -0300

    services responsive

--- src/app/servicios/ServiciosSection.tsx
+++ src/app/servicios/ServiciosSection.tsx
@@ -22,28 +22,32 @@ const ServiciosSection = () => {
             },
           }}
           viewport={{ margin: '0px 0px -300px 0px', once: true }}
-          className='bg-color-primary rounded overflow-hidden flex flex-col items-start md:flex-row md:items-center max-w-[700px]'
+          className='rounded overflow-hidden flex flex-col items-start md:flex-row md:items-'
         >
-          <div className='pl-10 pr-16 py-16'>
-            <div className='text-3xl sm:text-4xl lg:text-5xl font-aleo-font font-semibold mb-1 sm:mb-2'>
+          <div className='px-5 md:px-10 py-5 md:py-10 lg:py-16 max-w-[400px] sm:max-w-[450px] md:max-w-full md:w-[450px] lg:w-[600px] h-full bg-color-primary border-b-4 md:border-b-0 md:border-r-8 border-color-secondary-light'>
+            <div className='text-3xl sm:text-3xl lg:text-4xl font-aleo-font font-semibold mb-1 sm:mb-2'>
               <h4 className='flex flex-col gap-1'>
-                <span className='text-color-title-light'>Desayuno</span>
-                <span className='text-color-title-light'>Merienda</span>
+                <span className='text-color-title-light'>
+                  Desayunos y Meriendas
+                </span>
               </h4>
             </div>
-            <div className='flex flex-col gap-2 md:text-lg lg:text-xl w-full max-w-[380px] sm:max-w-max sm:w-[480px] md:w-[350px] lg:w-[500px] text-color-text'>
-              <ul className='flex flex-col gap-1 text-color-title-light/75'>
+            <div className='flex flex-col gap-4 md:text-lg lg:text-lg w-full max-w-[380px] sm:max-w-max sm:w-[420px] md:w-[350px] lg:w-[500px]'>
+              <p className='text-color-text-light'>
+                Despertá tus sentidos con nuestras delicias matutinas, ¡el café
+                perfecto y opciones saludables para arrancar con energía!
+              </p>
+              <ul className='grid grid-cols-2 text-sm sm:text-base md:text-lg text-color-title-light/60'>
                 <li>• Café</li>
                 <li>• Medialunas</li>
                 <li>• Tostadas</li>
                 <li>• Huevos revueltos</li>
                 <li>• Avocado Toast</li>
                 <li>• Jugos</li>
-                <li>• Tortas</li>
               </ul>
             </div>
           </div>
-          <div className='relative w-[300px] h-full'>
+          <div className='relative h-[220px] w-full md:w-[250px] lg:w-[300px] md:h-full'>
             <Image
               className='w-full h-full object-cover'
               src='/assets/servicios/servicio-1.webp'
@@ -64,16 +68,23 @@ const ServiciosSection = () => {
             },
           }}
           viewport={{ margin: '0px 0px -300px 0px', once: true }}
-          className='bg-color-primary rounded overflow-hidden flex flex-col items-start md:flex-row md:items-center'
+          className='rounded overflow-hidden flex flex-col items-start md:flex-row md:items-center'
         >
-          <div className='pl-10 pr-16 py-16 md:order-2'>
-            <div className='text-3xl sm:text-4xl lg:text-5xl font-aleo-font font-semibold mb-1 sm:mb-2'>
+          <div className='px-5 md:px-10 py-5 md:py-10 lg:py-16 max-w-[400px] sm:max-w-[450px] md:max-w-full md:w-[450px] lg:w-[600px] md:order-2 h-full bg-color-primary border-b-4 md:border-b-0 md:border-l-8 border-color-secondary-light'>
+            <div className='text-3xl sm:text-3xl lg:text-4xl font-aleo-font font-semibold mb-1 sm:mb-2'>
               <h4 className='flex flex-col gap-1'>
-                <span className='text-color-title-light'>Almuerzo</span>
+                <span className='text-color-title-light'>
+                  Almorzá con sabor y frescura
+                </span>
               </h4>
             </div>
-            <div className='flex flex-col gap-2 md:text-lg lg:text-xl w-full max-w-[380px] sm:max-w-max sm:w-[480px] md:w-[350px] lg:w-[500px] text-color-text'>
-              <ul className='flex flex-col gap-1 text-color-title-light/75'>
+            <div className='flex flex-col gap-4 md:text-lg lg:text-lg w-full max-w-[380px] sm:max-w-max sm:w-[420px] md:w-[350px] lg:w-[500px]'>
+              <p className='text-color-text-light'>
+                Disfrutá de nuestros almuerzos caseros, preparados con
+                ingredientes frescos y un toque único. ¡Ideal para una pausa en
+                tu día!
+              </p>
+              <ul className='grid grid-cols-2 text-sm sm:text-base md:text-lg text-color-title-light/60'>
                 <li>• Pastas</li>
                 <li>• Milanesas</li>
                 <li>• Sandwiches</li>
@@ -83,7 +94,7 @@ const ServiciosSection = () => {
               </ul>
             </div>
           </div>
-          <div className='relative w-[300px] h-full'>
+          <div className='relative h-[220px] w-full md:w-[250px] lg:w-[300px] md:h-full'>
             <Image
               className='w-full h-full object-cover'
               src='/assets/servicios/servicio-2.webp'
@@ -104,16 +115,23 @@ const ServiciosSection = () => {
             },
           }}
           viewport={{ margin: '0px 0px -300px 0px', once: true }}
-          className='bg-color-primary rounded overflow-hidden flex flex-col items-start md:flex-row md:items-center'
+          className=' rounded overflow-hidden flex flex-col items-start md:flex-row md:items-center'
         >
-          <div className='pl-10 pr-16 py-16'>
-            <div className='text-3xl sm:text-4xl lg:text-5xl font-aleo-font font-semibold mb-1 sm:mb-2'>
-              <h4>
-                <span className='text-color-title-light'>Postres</span>
+          <div className='px-5 md:px-10 py-5 md:py-10 lg:py-16 max-w-[400px] sm:max-w-[450px] md:max-w-full md:w-[450px] lg:w-[600px] h-full bg-color-primary border-b-4 md:border-b-0 md:border-r-8 border-color-secondary-light'>
+            <div className='text-3xl sm:text-3xl lg:text-4xl font-aleo-font font-semibold mb-1 sm:mb-2'>
+              <h4 className='flex flex-col gap-1'>
+                <span className='text-color-title-light'>
+                  Postres caseros y más
+                </span>
               </h4>
             </div>
-            <div className='flex flex-col gap-2 md:text-lg lg:text-xl w-full max-w-[380px] sm:max-w-max sm:w-[480px] md:w-[350px] lg:w-[500px] text-color-text'>
-              <ul className='flex flex-col gap-1 text-color-title-light/75'>
+            <div className='flex flex-col gap-4 md:text-lg lg:text-lg w-full max-w-[380px] sm:max-w-max sm:w-[420px] md:w-[350px] lg:w-[500px]'>
+              <p className='text-color-text-light'>
+                Deleitate con nuestros irresistibles postres. Desde clásicos que
+                enamoran hasta creaciones únicas, cada bocado es una dulce
+                celebración para tu paladar. ¡No te vayas sin probarlos!
+              </p>
+              <ul className='grid grid-cols-2 text-sm sm:text-base md:text-lg text-color-title-light/60'>
                 <li>• Ensalada de fruta</li>
                 <li>• Panqueques</li>
                 <li>• Brownie</li>
@@ -123,7 +141,7 @@ const ServiciosSection = () => {
               </ul>
             </div>
           </div>
-          <div className='relative w-[300px] h-full'>
+          <div className='relative h-[220px] w-full md:w-[250px] lg:w-[300px] md:h-full'>
             <Image
               className='w-full h-full object-cover'
               src='/assets/servicios/servicio-3.webp'
