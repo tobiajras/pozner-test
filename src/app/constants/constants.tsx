66a7c697a340e71d016774b5cd5640fcad93b286
tobyajras <tobyajras@gmail.com>
2024-10-08 15:50:49 -0300

    mvp habito-cafe

--- src/app/constants/constants.tsx
+++ src/app/constants/constants.tsx
@@ -1,35 +1,35 @@
 export const company = {
-  id: 'panela-cafe',
-  name: 'Panela Café',
-  adress: 'Rosales 1568',
-  city: 'Adrogué',
-  telephone: '1151035747',
-  email: null,
-  instagram: 'panela.ar',
-  facebook: null,
-  whatsapp: '1151035747',
+  id: 'habito-cafe',
+  name: 'Hábito Café',
+  adress: 'México 1152',
+  city: 'CABA',
+  telephone: '1164907828',
+  email: 'habitocafeteria@gmail.com',
+  instagram: 'habito.cafe',
+  facebook: 'habito.tomabuencafe',
+  whatsapp: '1164907828',
   googlemaps:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13105.18120454146!2d-58.38533520000001!3d-34.7985118!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcd362fc99033f%3A0x1ecd0659d97974cb!2sPanela%20Caf%C3%A9!5e0!3m2!1sen!2sar!4v1728194012145!5m2!1sen!2sar',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.555880620415!2d-58.382409100000004!3d-34.6153903!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccad79cce3563%3A0xf34b22bd7f2a7491!2zSMOhYml0byBDYWbDqQ!5e0!3m2!1ses-419!2sar!4v1728410328680!5m2!1ses-419!2sar',
   menu: null,
   openDays: [
-    { day: 'Lunes', hours: ['07:00 a 20:00'] },
-    { day: 'Martes', hours: ['07:00 a 20:00'] },
-    { day: 'Miércoles', hours: ['07:00 a 20:00'] },
-    { day: 'Jueves', hours: ['07:00 a 20:00'] },
-    { day: 'Viernes', hours: ['07:00 a 20:00'] },
-    { day: 'Sábado', hours: ['08:00 a 20:00'] },
-    { day: 'Domingo', hours: ['10:00 a 20:00'] },
+    { day: 'Lunes', hours: ['09:00 a 19:00'] },
+    { day: 'Martes', hours: ['09:00 a 19:00'] },
+    { day: 'Miércoles', hours: ['09:00 a 19:00'] },
+    { day: 'Jueves', hours: ['09:00 a 19:00'] },
+    { day: 'Viernes', hours: ['09:00 a 19:00'] },
+    { day: 'Sábado', hours: ['10:00 a 18:00'] },
+    // { day: 'Domingo', hours: ['10:00 a 18:00'] },
   ],
   footer: 'Café de Especialidad, dedicación y pasión por lo que hacemos.',
 };
 
 export const nosotros = {
-  text: 'En Panela Café, nos enorgullecemos de nuestra larga trayectoria con los más altos estándares de calidad. Con amplia experiencia en el rubro, nos hemos comprometido a brindar un ambiente ideal para que disfrutes de nuestra gastronomía con amigos y familiares.',
+  text: 'En Hábito Café, nos enorgullecemos de nuestra larga trayectoria con los más altos estándares de calidad. Con amplia experiencia en el rubro, nos hemos comprometido a brindar un ambiente ideal para que disfrutes de nuestra gastronomía con amigos y familiares.',
 };
 
 export const metadataCompany = {
-  metadataBase: 'https://panela-cafe.vercel.app/',
-  title: 'Panela Café - Café de Especialidad',
+  metadataBase: 'https://habito-cafe.vercel.app/',
+  title: 'Hábito Café - Café de Especialidad',
   description: 'Café de Especialidad, dedicación y pasión por lo que hacemos.',
 };
 
