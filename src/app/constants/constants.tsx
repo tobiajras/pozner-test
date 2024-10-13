87b5f0511b7b98b7d2373e880d4b62b9c9a9cb9b
tobyajras <tobyajras@gmail.com>
2024-10-13 18:56:58 -0300

    mvp lando-cafe

--- src/app/constants/constants.tsx
+++ src/app/constants/constants.tsx
@@ -1,35 +1,35 @@
 export const company = {
-  id: 'begonia-cafe',
-  name: 'Begonia Café',
-  adress: 'Adrogué 1180',
+  id: 'lando-cafe',
+  name: 'Landó Café',
+  adress: 'Diagonal Brown 1799',
   city: 'Adrogué',
-  telephone: null,
-  email: null,
-  instagram: 'begonia.bar',
-  facebook: null,
-  whatsapp: null,
+  telephone: '1125156462',
+  email: 'landocafeadrogue@gmail.com',
+  instagram: 'landocafe.adrogue',
+  facebook: 'Landó-Café-Adrogué',
+  whatsapp: '1125156462',
   googlemaps:
-    'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d204.7688755656194!2d-58.39056174254382!3d-34.79834300639544!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcd3b5b4b889e7%3A0x2feb4d8aa714e229!2sBegonia%20Cafe%26Deli!5e0!3m2!1ses-419!2sar!4v1728616704090!5m2!1ses-419!2sar',
-  menu: null,
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d409.5378433517218!2d-58.3807867344538!3d-34.7983244417411!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcd3f8e49d32d9%3A0x470e3090ee10203b!2zTGFuZMOzIENhZsOp!5e0!3m2!1ses-419!2sar!4v1728618842414!5m2!1ses-419!2sar',
+  menu: 'https://menu.maxirest.com/42207',
   openDays: [
-    { day: 'Lunes', hours: ['08:00 a 22:00'] },
-    { day: 'Martes', hours: ['08:00 a 22:00'] },
-    { day: 'Miércoles', hours: ['08:00 a 22:00'] },
-    { day: 'Jueves', hours: ['08:00 a 22:00'] },
-    { day: 'Viernes', hours: ['08:00 a 22:00'] },
-    { day: 'Sábado', hours: ['08:00 a 22:00'] },
-    // { day: 'Domingo', hours: ['10:00 a 22:00'] },
+    // { day: 'Lunes', hours: ['08:30 a 20:00'] },
+    { day: 'Martes', hours: ['08:30 a 20:00'] },
+    { day: 'Miércoles', hours: ['08:30 a 20:00'] },
+    { day: 'Jueves', hours: ['08:30 a 20:00'] },
+    { day: 'Viernes', hours: ['08:30 a 20:00'] },
+    { day: 'Sábado', hours: ['08:30 a 20:00'] },
+    { day: 'Domingo', hours: ['08:30 a 20:00'] },
   ],
   footer: 'Café de Especialidad, dedicación y pasión por lo que hacemos.',
 };
 
 export const nosotros = {
-  text: 'En Begonia Café, nos enorgullecemos de nuestra larga trayectoria con los más altos estándares de calidad. Con amplia experiencia en el rubro, nos hemos comprometido a brindar un ambiente ideal para que disfrutes de nuestra gastronomía con amigos y familiares.',
+  text: 'En Landó Café, nos enorgullecemos de nuestra larga trayectoria con los más altos estándares de calidad. Con amplia experiencia en el rubro, nos hemos comprometido a brindar un ambiente ideal para que disfrutes de nuestra gastronomía con amigos y familiares.',
 };
 
 export const metadataCompany = {
-  metadataBase: 'https://begonia-cafe.vercel.app/',
-  title: 'Begonia Café - Café de Especialidad',
+  metadataBase: 'https://lando-cafe.vercel.app/',
+  title: 'Landó Café - Café de Especialidad',
   description: 'Café de Especialidad, dedicación y pasión por lo que hacemos.',
 };
 
