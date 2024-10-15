91862bfd38c9c1609722049c5e08fda4eaf4a7b1
tobiajras <tobyajras@gmail.com>
2024-10-15 02:32:09 -0300

    mvp gokotta-cafe

--- src/app/constants/constants.tsx
+++ src/app/constants/constants.tsx
@@ -1,35 +1,35 @@
 export const company = {
-  id: 'malapalabra-cafe',
-  name: 'Mala Palabra Café',
-  adress: 'Nueva York 3777',
-  city: 'Villa Devoto',
+  id: 'gokotta-cafe',
+  name: 'Gokotta Café',
+  adress: 'Arévalo 2870',
+  city: 'Cañitas',
   telephone: null,
   email: null,
-  instagram: 'malapalabra.cafe',
+  instagram: 'gokotta__cafe',
   facebook: null,
   whatsapp: null,
   googlemaps:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13136.8927807682!2d-58.50906309999999!3d-34.598517199999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb7ebfd536e71%3A0xcd497107214c7a29!2sMala%20Palabra%20Caf%C3%A9%20de%20Especialidad!5e0!3m2!1ses-419!2sar!4v1728700997188!5m2!1ses-419!2sar',
-  menu: 'https://www.canva.com/design/DAFvexubNa8/BePtm29sqIYS8Co_V0Gocg/edit',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d210070.74573354103!2d-58.60067702187504!3d-34.645726382516386!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb5c5e71cb6a7%3A0xc5afa54618551d09!2sGokotta%20Caf%C3%A9!5e0!3m2!1ses-419!2sar!4v1728763080715!5m2!1ses-419!2sar',
+  menu: 'https://menu.fu.do/gokotta/qr-menu',
   openDays: [
-    { day: 'Lunes', hours: ['08:00 a 20:00'] },
-    { day: 'Martes', hours: ['08:00 a 20:00'] },
-    { day: 'Miércoles', hours: ['08:00 a 20:00'] },
-    { day: 'Jueves', hours: ['08:00 a 20:00'] },
-    { day: 'Viernes', hours: ['08:00 a 20:00'] },
-    { day: 'Sábado', hours: ['08:00 a 20:00'] },
-    { day: 'Domingo', hours: ['08:00 a 20:00'] },
+    { day: 'Lunes', hours: ['09:00 a 20:00'] },
+    { day: 'Martes', hours: ['09:00 a 20:00'] },
+    { day: 'Miércoles', hours: ['09:00 a 20:00'] },
+    { day: 'Jueves', hours: ['09:00 a 20:00'] },
+    { day: 'Viernes', hours: ['09:00 a 20:00'] },
+    { day: 'Sábado', hours: ['09:00 a 20:00'] },
+    { day: 'Domingo', hours: ['09:00 a 20:00'] },
   ],
   footer: 'Café de Especialidad, dedicación y pasión por lo que hacemos.',
 };
 
 export const nosotros = {
-  text: 'En Mala Palabra Café, nos enorgullecemos de nuestra larga trayectoria con los más altos estándares de calidad. Con amplia experiencia en el rubro, nos hemos comprometido a brindar un ambiente ideal para que disfrutes de nuestra gastronomía con amigos y familiares.',
+  text: 'En Gokotta Café, nos enorgullecemos de nuestra larga trayectoria con los más altos estándares de calidad. Con amplia experiencia en el rubro, nos hemos comprometido a brindar un ambiente ideal para que disfrutes de nuestra gastronomía con amigos y familiares.',
 };
 
 export const metadataCompany = {
-  metadataBase: 'https://malapalabra-cafe.vercel.app/',
-  title: 'Mala Palabra Café - Café de Especialidad',
+  metadataBase: 'https://gokotta-cafe.vercel.app/',
+  title: 'Gokotta Café - Café de Especialidad',
   description: 'Café de Especialidad, dedicación y pasión por lo que hacemos.',
 };
 
