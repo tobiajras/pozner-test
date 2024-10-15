9b383675b513e6d124bec85f22e38392ddaa1af9
tobiajras <tobyajras@gmail.com>
2024-10-15 00:41:07 -0300

    mvp bouque-cafe

--- src/app/constants/constants.tsx
+++ src/app/constants/constants.tsx
@@ -1,35 +1,35 @@
 export const company = {
-  id: 'elsa-cafe',
-  name: 'Elsa Café',
-  adress: 'El Salvador 5749',
-  city: 'Palermo',
-  telephone: null,
+  id: 'bouque-cafe',
+  name: 'Bouqué Café',
+  adress: 'Av. Forest 1300',
+  city: 'Colegiales',
+  telephone: '1171714574',
   email: null,
-  instagram: 'elsa.cafe_',
+  instagram: 'bouquespecialtycoffee',
   facebook: null,
-  whatsapp: null,
+  whatsapp: '1171714574',
   googlemaps:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.876341655073!2d-58.436754477769114!3d-34.58199536324353!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb5bf10759b09%3A0xe9eb7c825be79cce!2sELSA%20CAFE%20%26%20DESPENSA!5e0!3m2!1ses-419!2sar!4v1728670753376!5m2!1ses-419!2sar',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d205.31694255534964!2d-58.46174021336981!3d-34.577067948704865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb5dce3a52bfd%3A0xd696db3b72ea991!2sBOUQUE!5e0!3m2!1ses-419!2sar!4v1728671917988!5m2!1ses-419!2sar',
   menu: null,
   openDays: [
-    { day: 'Lunes', hours: ['08:00 a 19:30'] },
-    { day: 'Martes', hours: ['08:00 a 19:30'] },
-    { day: 'Miércoles', hours: ['08:00 a 19:30'] },
-    { day: 'Jueves', hours: ['08:00 a 19:30'] },
-    { day: 'Viernes', hours: ['08:00 a 19:30'] },
-    { day: 'Sábado', hours: ['08:30 a 20:00'] },
-    { day: 'Domingo', hours: ['08:30 a 20:00'] },
+    { day: 'Lunes', hours: ['08:00 a 20:00'] },
+    { day: 'Martes', hours: ['08:00 a 20:00'] },
+    { day: 'Miércoles', hours: ['08:00 a 20:00'] },
+    { day: 'Jueves', hours: ['08:00 a 20:00'] },
+    { day: 'Viernes', hours: ['08:00 a 20:00'] },
+    { day: 'Sábado', hours: ['08:00 a 20:00'] },
+    { day: 'Domingo', hours: ['08:00 a 20:00'] },
   ],
   footer: 'Café de Especialidad, dedicación y pasión por lo que hacemos.',
 };
 
 export const nosotros = {
-  text: 'En Elsa Café, nos enorgullecemos de nuestra larga trayectoria con los más altos estándares de calidad. Con amplia experiencia en el rubro, nos hemos comprometido a brindar un ambiente ideal para que disfrutes de nuestra gastronomía con amigos y familiares.',
+  text: 'En Bouqué Café, nos enorgullecemos de nuestra larga trayectoria con los más altos estándares de calidad. Con amplia experiencia en el rubro, nos hemos comprometido a brindar un ambiente ideal para que disfrutes de nuestra gastronomía con amigos y familiares.',
 };
 
 export const metadataCompany = {
-  metadataBase: 'https://elsa-cafe.vercel.app/',
-  title: 'Elsa Café - Café de Especialidad',
+  metadataBase: 'https://bouque-cafe.vercel.app/',
+  title: 'Bouqué Café - Café de Especialidad',
   description: 'Café de Especialidad, dedicación y pasión por lo que hacemos.',
 };
 
