a4427ac8b5536cf48a187ca491f8377fce2be206
tobiajras <tobyajras@gmail.com>
2024-10-15 00:54:12 -0300

    mvp jauria-cafe

--- src/app/constants/constants.tsx
+++ src/app/constants/constants.tsx
@@ -1,35 +1,35 @@
 export const company = {
-  id: 'bouque-cafe',
-  name: 'Bouqué Café',
-  adress: 'Av. Forest 1300',
+  id: 'jauria-cafe',
+  name: 'Jauria Café',
+  adress: 'Céspedes 2457',
   city: 'Colegiales',
-  telephone: '1171714574',
+  telephone: null,
   email: null,
-  instagram: 'bouquespecialtycoffee',
+  instagram: 'jauriacafe__',
   facebook: null,
-  whatsapp: '1171714574',
+  whatsapp: null,
   googlemaps:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d205.31694255534964!2d-58.46174021336981!3d-34.577067948704865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb5dce3a52bfd%3A0xd696db3b72ea991!2sBOUQUE!5e0!3m2!1ses-419!2sar!4v1728671917988!5m2!1ses-419!2sar',
-  menu: null,
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d209944.31407215388!2d-58.57542027187499!3d-34.69559658944559!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb5001392dbcd%3A0x2bbb7b50c5b654fe!2sJaur%C3%ADa%20Cafe!5e0!3m2!1ses-419!2sar!4v1728673255966!5m2!1ses-419!2sar',
+  menu: 'https://drive.google.com/drive/folders/1Med9mrf15dkG2nGI4BDxqd8s4m7wTqXP?fbclid=PAZXh0bgNhZW0CMTEAAabCJpgS9Fz_8apkbs38sGQ7dlnnpszzOotw6YzdDS1jRsXG3w_GX3Il-D4_aem_3oPOmdBUv6j53kNmDjaGzQ',
   openDays: [
     { day: 'Lunes', hours: ['08:00 a 20:00'] },
     { day: 'Martes', hours: ['08:00 a 20:00'] },
     { day: 'Miércoles', hours: ['08:00 a 20:00'] },
     { day: 'Jueves', hours: ['08:00 a 20:00'] },
     { day: 'Viernes', hours: ['08:00 a 20:00'] },
-    { day: 'Sábado', hours: ['08:00 a 20:00'] },
-    { day: 'Domingo', hours: ['08:00 a 20:00'] },
+    { day: 'Sábado', hours: ['09:00 a 20:00'] },
+    { day: 'Domingo', hours: ['09:00 a 20:00'] },
   ],
   footer: 'Café de Especialidad, dedicación y pasión por lo que hacemos.',
 };
 
 export const nosotros = {
-  text: 'En Bouqué Café, nos enorgullecemos de nuestra larga trayectoria con los más altos estándares de calidad. Con amplia experiencia en el rubro, nos hemos comprometido a brindar un ambiente ideal para que disfrutes de nuestra gastronomía con amigos y familiares.',
+  text: 'En Jauria Café, nos enorgullecemos de nuestra larga trayectoria con los más altos estándares de calidad. Con amplia experiencia en el rubro, nos hemos comprometido a brindar un ambiente ideal para que disfrutes de nuestra gastronomía con amigos y familiares.',
 };
 
 export const metadataCompany = {
-  metadataBase: 'https://bouque-cafe.vercel.app/',
-  title: 'Bouqué Café - Café de Especialidad',
+  metadataBase: 'https://jauria-cafe.vercel.app/',
+  title: 'Jauria Café - Café de Especialidad',
   description: 'Café de Especialidad, dedicación y pasión por lo que hacemos.',
 };
 
