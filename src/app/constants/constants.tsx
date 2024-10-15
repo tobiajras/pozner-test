fe16783da6b7541796f2dc870cda96d526c69ee6
tobiajras <tobyajras@gmail.com>
2024-10-15 00:18:30 -0300

    mvp elsa-cafe

--- src/app/constants/constants.tsx
+++ src/app/constants/constants.tsx
@@ -1,22 +1,22 @@
 export const company = {
-  id: 'lando-cafe',
-  name: 'Landó Café',
-  adress: 'Diagonal Brown 1799',
-  city: 'Adrogué',
-  telephone: '1125156462',
-  email: 'landocafeadrogue@gmail.com',
-  instagram: 'landocafe.adrogue',
-  facebook: 'Landó-Café-Adrogué',
-  whatsapp: '1125156462',
+  id: 'elsa-cafe',
+  name: 'Elsa Café',
+  adress: 'El Salvador 5749',
+  city: 'Palermo',
+  telephone: null,
+  email: null,
+  instagram: 'elsa.cafe_',
+  facebook: null,
+  whatsapp: null,
   googlemaps:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d409.5378433517218!2d-58.3807867344538!3d-34.7983244417411!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcd3f8e49d32d9%3A0x470e3090ee10203b!2zTGFuZMOzIENhZsOp!5e0!3m2!1ses-419!2sar!4v1728618842414!5m2!1ses-419!2sar',
-  menu: 'https://menu.maxirest.com/42207',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.876341655073!2d-58.436754477769114!3d-34.58199536324353!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb5bf10759b09%3A0xe9eb7c825be79cce!2sELSA%20CAFE%20%26%20DESPENSA!5e0!3m2!1ses-419!2sar!4v1728670753376!5m2!1ses-419!2sar',
+  menu: null,
   openDays: [
-    // { day: 'Lunes', hours: ['08:30 a 20:00'] },
-    { day: 'Martes', hours: ['08:30 a 20:00'] },
-    { day: 'Miércoles', hours: ['08:30 a 20:00'] },
-    { day: 'Jueves', hours: ['08:30 a 20:00'] },
-    { day: 'Viernes', hours: ['08:30 a 20:00'] },
+    { day: 'Lunes', hours: ['08:00 a 19:30'] },
+    { day: 'Martes', hours: ['08:00 a 19:30'] },
+    { day: 'Miércoles', hours: ['08:00 a 19:30'] },
+    { day: 'Jueves', hours: ['08:00 a 19:30'] },
+    { day: 'Viernes', hours: ['08:00 a 19:30'] },
     { day: 'Sábado', hours: ['08:30 a 20:00'] },
     { day: 'Domingo', hours: ['08:30 a 20:00'] },
   ],
@@ -24,12 +24,12 @@ export const company = {
 };
 
 export const nosotros = {
-  text: 'En Landó Café, nos enorgullecemos de nuestra larga trayectoria con los más altos estándares de calidad. Con amplia experiencia en el rubro, nos hemos comprometido a brindar un ambiente ideal para que disfrutes de nuestra gastronomía con amigos y familiares.',
+  text: 'En Elsa Café, nos enorgullecemos de nuestra larga trayectoria con los más altos estándares de calidad. Con amplia experiencia en el rubro, nos hemos comprometido a brindar un ambiente ideal para que disfrutes de nuestra gastronomía con amigos y familiares.',
 };
 
 export const metadataCompany = {
-  metadataBase: 'https://lando-cafe.vercel.app/',
-  title: 'Landó Café - Café de Especialidad',
+  metadataBase: 'https://elsa-cafe.vercel.app/',
+  title: 'Elsa Café - Café de Especialidad',
   description: 'Café de Especialidad, dedicación y pasión por lo que hacemos.',
 };
 
