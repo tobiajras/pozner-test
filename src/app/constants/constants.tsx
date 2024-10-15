99f5cc40f41c8c3a760626c93a110f49f03808e2
tobiajras <tobyajras@gmail.com>
2024-10-15 02:44:18 -0300

    mvp bahari-cafe

--- src/app/constants/constants.tsx
+++ src/app/constants/constants.tsx
@@ -1,16 +1,16 @@
 export const company = {
-  id: 'gokotta-cafe',
-  name: 'Gokotta Café',
-  adress: 'Arévalo 2870',
-  city: 'Cañitas',
-  telephone: null,
+  id: 'bahari-cafe',
+  name: 'Bahari Café',
+  adress: 'Emilio Zola 6230',
+  city: 'Wilde',
+  telephone: '1133310506',
   email: null,
-  instagram: 'gokotta__cafe',
+  instagram: 'bahari.cafe',
   facebook: null,
-  whatsapp: null,
+  whatsapp: '1133310506',
   googlemaps:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d210070.74573354103!2d-58.60067702187504!3d-34.645726382516386!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb5c5e71cb6a7%3A0xc5afa54618551d09!2sGokotta%20Caf%C3%A9!5e0!3m2!1ses-419!2sar!4v1728763080715!5m2!1ses-419!2sar',
-  menu: 'https://menu.fu.do/gokotta/qr-menu',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d209931.84510903273!2d-58.477555921874995!3d-34.7005115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a333df1a917311%3A0xcca181a9f215a774!2sBahari%20Caf%C3%A9%20de%20Especialidad!5e0!3m2!1ses-419!2sar!4v1728766207779!5m2!1ses-419!2sar',
+  menu: 'https://drive.google.com/file/d/1h6c_FfZzyoyFwuwKu5kf6GNDDLaYgR_8/view?fbclid=PAZXh0bgNhZW0CMTEAAaat7QZlklgTylZENTmdsn6X87LHUImt9re0-1Yh0xoo-LNRh40xBo9elvE_aem_oWnfbvrgnKIMnPxCNi7Lnw',
   openDays: [
     { day: 'Lunes', hours: ['09:00 a 20:00'] },
     { day: 'Martes', hours: ['09:00 a 20:00'] },
@@ -18,18 +18,18 @@ export const company = {
     { day: 'Jueves', hours: ['09:00 a 20:00'] },
     { day: 'Viernes', hours: ['09:00 a 20:00'] },
     { day: 'Sábado', hours: ['09:00 a 20:00'] },
-    { day: 'Domingo', hours: ['09:00 a 20:00'] },
+    { day: 'Domingo', hours: ['09:00 a 13:00'] },
   ],
   footer: 'Café de Especialidad, dedicación y pasión por lo que hacemos.',
 };
 
 export const nosotros = {
-  text: 'En Gokotta Café, nos enorgullecemos de nuestra larga trayectoria con los más altos estándares de calidad. Con amplia experiencia en el rubro, nos hemos comprometido a brindar un ambiente ideal para que disfrutes de nuestra gastronomía con amigos y familiares.',
+  text: 'En Bahari Café, nos enorgullecemos de nuestra larga trayectoria con los más altos estándares de calidad. Con amplia experiencia en el rubro, nos hemos comprometido a brindar un ambiente ideal para que disfrutes de nuestra gastronomía con amigos y familiares.',
 };
 
 export const metadataCompany = {
-  metadataBase: 'https://gokotta-cafe.vercel.app/',
-  title: 'Gokotta Café - Café de Especialidad',
+  metadataBase: 'https://bahari-cafe.vercel.app/',
+  title: 'Bahari Café - Café de Especialidad',
   description: 'Café de Especialidad, dedicación y pasión por lo que hacemos.',
 };
 
