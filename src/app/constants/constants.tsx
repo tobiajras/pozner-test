cf2053317db1ea5bf0202c7eccec90d0f237a2b4
tobiajras <tobyajras@gmail.com>
2024-10-15 01:09:33 -0300

    mvp panda-cafe

--- src/app/constants/constants.tsx
+++ src/app/constants/constants.tsx
@@ -1,35 +1,35 @@
 export const company = {
-  id: 'jauria-cafe',
-  name: 'Jauria Café',
-  adress: 'Céspedes 2457',
-  city: 'Colegiales',
-  telephone: null,
+  id: 'panda-coffee',
+  name: 'Panda Coffee',
+  adress: 'Ituzaingo 333',
+  city: 'San Isidro',
+  telephone: '1123922251',
   email: null,
-  instagram: 'jauriacafe__',
+  instagram: 'pandacoffee.ar',
   facebook: null,
-  whatsapp: null,
+  whatsapp: '1123922251',
   googlemaps:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d209944.31407215388!2d-58.57542027187499!3d-34.69559658944559!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb5001392dbcd%3A0x2bbb7b50c5b654fe!2sJaur%C3%ADa%20Cafe!5e0!3m2!1ses-419!2sar!4v1728673255966!5m2!1ses-419!2sar',
-  menu: 'https://drive.google.com/drive/folders/1Med9mrf15dkG2nGI4BDxqd8s4m7wTqXP?fbclid=PAZXh0bgNhZW0CMTEAAabCJpgS9Fz_8apkbs38sGQ7dlnnpszzOotw6YzdDS1jRsXG3w_GX3Il-D4_aem_3oPOmdBUv6j53kNmDjaGzQ',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13157.32306849437!2d-58.512258800000005!3d-34.4691324!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb1703d11276b%3A0xed74c2ebe47bf44d!2sPanda%20Coffee!5e0!3m2!1ses-419!2sar!4v1728693924876!5m2!1ses-419!2sar',
+  menu: 'https://www.canva.com/design/DAGC4QShem4/Som5XVlzYy-MwOCpE3v4Iw/view?utm_content=DAGC4QShem4&utm_campaign=designshare&utm_medium=link&utm_source=editor',
   openDays: [
-    { day: 'Lunes', hours: ['08:00 a 20:00'] },
-    { day: 'Martes', hours: ['08:00 a 20:00'] },
-    { day: 'Miércoles', hours: ['08:00 a 20:00'] },
-    { day: 'Jueves', hours: ['08:00 a 20:00'] },
-    { day: 'Viernes', hours: ['08:00 a 20:00'] },
-    { day: 'Sábado', hours: ['09:00 a 20:00'] },
-    { day: 'Domingo', hours: ['09:00 a 20:00'] },
+    { day: 'Lunes', hours: ['09:00 a 18:30'] },
+    { day: 'Martes', hours: ['09:00 a 18:30'] },
+    { day: 'Miércoles', hours: ['09:00 a 18:30'] },
+    { day: 'Jueves', hours: ['09:00 a 18:30'] },
+    { day: 'Viernes', hours: ['09:00 a 18:30'] },
+    { day: 'Sábado', hours: ['09:00 a 18:30'] },
+    // { day: 'Domingo', hours: ['09:00 a 18:30'] },
   ],
   footer: 'Café de Especialidad, dedicación y pasión por lo que hacemos.',
 };
 
 export const nosotros = {
-  text: 'En Jauria Café, nos enorgullecemos de nuestra larga trayectoria con los más altos estándares de calidad. Con amplia experiencia en el rubro, nos hemos comprometido a brindar un ambiente ideal para que disfrutes de nuestra gastronomía con amigos y familiares.',
+  text: 'En Panda Café, nos enorgullecemos de nuestra larga trayectoria con los más altos estándares de calidad. Con amplia experiencia en el rubro, nos hemos comprometido a brindar un ambiente ideal para que disfrutes de nuestra gastronomía con amigos y familiares.',
 };
 
 export const metadataCompany = {
-  metadataBase: 'https://jauria-cafe.vercel.app/',
-  title: 'Jauria Café - Café de Especialidad',
+  metadataBase: 'https://panda-cafe.vercel.app/',
+  title: 'Panda Café - Café de Especialidad',
   description: 'Café de Especialidad, dedicación y pasión por lo que hacemos.',
 };
 
