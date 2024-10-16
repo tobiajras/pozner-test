ad34cc5d4ecc97990f2c0d1ada406c3bf17fb8c5
tobiajras <tobyajras@gmail.com>
2024-10-16 13:07:31 -0300

    mvp hilario-cafe

--- src/app/constants/constants.tsx
+++ src/app/constants/constants.tsx
@@ -1,35 +1,35 @@
 export const company = {
-  id: 'nila-cafe',
-  name: 'Nila Café',
-  adress: 'Bonpland 2050',
-  city: 'Palermo',
-  telephone: '1164586165',
+  id: 'hilario-cafe',
+  name: 'Hilario Café',
+  adress: 'Colon 140',
+  city: 'Quilmes',
+  telephone: '1165934159',
   email: null,
-  instagram: 'nila.cafepalermo',
+  instagram: 'hilario.cafe',
   facebook: null,
-  whatsapp: '1164586165',
+  whatsapp: '1165934159',
   googlemaps:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13136.325727310772!2d-58.383853099999996!3d-34.6021023!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccb53a2d0429b%3A0x13bfcfa03f3e6b58!2sCaf%C3%A9%20Boheme%20-%20Caf%C3%A9%20de%20Especialidad!5e0!3m2!1sen!2sar!4v1728200216369!5m2!1sen!2sar',
-  menu: 'https://drive.google.com/file/d/1_7-ZI1Hg9PWMi7cPWdLoBkVd_EwzCX_t/view?fbclid=PAZXh0bgNhZW0CMTEAAaY-uN9X5_wjmlgIAxfeMa7MD40tdkLbFpdeJO6YVnL1t3wHG-UnAN0MyW8_aem_Jh_zZ0Dmd3tkqTmPKxdadg',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13117.743278173746!2d-58.26096237596943!3d-34.7194074034644!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a32f882b1fa887%3A0x73838fdba02085e6!2sHilario%20Caf%C3%A9%20de%20Especialidad!5e0!3m2!1ses-419!2sar!4v1728789805666!5m2!1ses-419!2sar',
+  menu: 'https://menu.fu.do/hilariocafedeespecialidad/qr-menu',
   openDays: [
-    { day: 'Lunes', hours: ['08:30 a 20:00'] },
-    // { day: 'Martes', hours: ['08:30 a 20:00'] },
-    { day: 'Miércoles', hours: ['08:30 a 20:00'] },
-    { day: 'Jueves', hours: ['08:30 a 20:00'] },
-    { day: 'Viernes', hours: ['08:30 a 20:00'] },
-    { day: 'Sábado', hours: ['09:00 a 20:00'] },
-    { day: 'Domingo', hours: ['09:00 a 20:00'] },
+    { day: 'Lunes', hours: ['08:00 a 20:00'] },
+    { day: 'Martes', hours: ['08:00 a 20:00'] },
+    { day: 'Miércoles', hours: ['08:00 a 20:00'] },
+    { day: 'Jueves', hours: ['08:00 a 20:00'] },
+    { day: 'Viernes', hours: ['08:00 a 20:00'] },
+    { day: 'Sábado', hours: ['09:30 a 20:00'] },
+    { day: 'Domingo', hours: ['16:30 a 20:00'] },
   ],
   footer: 'Café de Especialidad, dedicación y pasión por lo que hacemos.',
 };
 
 export const nosotros = {
-  text: 'En Nila Café, nos enorgullecemos de nuestra larga trayectoria con los más altos estándares de calidad. Con amplia experiencia en el rubro, nos hemos comprometido a brindar un ambiente ideal para que disfrutes de nuestra gastronomía con amigos y familiares.',
+  text: 'En Hilario Café, nos enorgullecemos de nuestra larga trayectoria con los más altos estándares de calidad. Con amplia experiencia en el rubro, nos hemos comprometido a brindar un ambiente ideal para que disfrutes de nuestra gastronomía con amigos y familiares.',
 };
 
 export const metadataCompany = {
-  metadataBase: 'https://nila-cafe.vercel.app/',
-  title: 'Nila Café - Café de Especialidad',
+  metadataBase: 'https://hilario-cafe.vercel.app/',
+  title: 'Hilario Café - Café de Especialidad',
   description: 'Café de Especialidad, dedicación y pasión por lo que hacemos.',
 };
 
