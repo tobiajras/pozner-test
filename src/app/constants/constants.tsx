f6060dfbdab100caea08500862cef1855bc14447
tobiajras <tobyajras@gmail.com>
2024-10-16 15:20:41 -0300

    mvp chois-cafe

--- src/app/constants/constants.tsx
+++ src/app/constants/constants.tsx
@@ -1,37 +1,64 @@
 export const company = {
-  id: 'hilario-cafe',
-  name: 'Hilario Café',
-  adress: 'Colon 140',
-  city: 'Quilmes',
-  telephone: '1165934159',
+  id: 'chois-cafe',
+  name: 'Choi’s Café',
+  adress: null,
+  city: null,
+  telephone: null,
   email: null,
-  instagram: 'hilario.cafe',
+  instagram: 'choiscafe_',
   facebook: null,
-  whatsapp: '1165934159',
+  whatsapp: null,
   googlemaps:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13117.743278173746!2d-58.26096237596943!3d-34.7194074034644!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a32f882b1fa887%3A0x73838fdba02085e6!2sHilario%20Caf%C3%A9%20de%20Especialidad!5e0!3m2!1ses-419!2sar!4v1728789805666!5m2!1ses-419!2sar',
-  menu: 'https://menu.fu.do/hilariocafedeespecialidad/qr-menu',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13132.039548257635!2d-58.481960005531036!3d-34.629190436028594!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcc96c42e2e521%3A0xe54f73e2a3724079!2sChoi&#39;s%20%26%20Cafe!5e0!3m2!1ses-419!2sar!4v1728793792504!5m2!1ses-419!2sar',
+  menu: 'https://pedix.app/choiscafe/categoria/IvAWVDzKtInNIIrt5MPN',
   openDays: [
-    { day: 'Lunes', hours: ['08:00 a 20:00'] },
-    { day: 'Martes', hours: ['08:00 a 20:00'] },
-    { day: 'Miércoles', hours: ['08:00 a 20:00'] },
-    { day: 'Jueves', hours: ['08:00 a 20:00'] },
-    { day: 'Viernes', hours: ['08:00 a 20:00'] },
-    { day: 'Sábado', hours: ['09:30 a 20:00'] },
-    { day: 'Domingo', hours: ['16:30 a 20:00'] },
+    { day: 'Lunes', hours: ['07:00 a 20:00'] },
+    { day: 'Martes', hours: ['07:00 a 20:00'] },
+    { day: 'Miércoles', hours: ['07:00 a 20:00'] },
+    { day: 'Jueves', hours: ['07:00 a 20:00'] },
+    { day: 'Viernes', hours: ['07:00 a 20:00'] },
+    { day: 'Sábado', hours: ['07:00 a 20:00'] },
+    { day: 'Domingo', hours: ['08:00 a 16:00'] },
   ],
   footer: 'Café de Especialidad, dedicación y pasión por lo que hacemos.',
 };
 
 export const nosotros = {
-  text: 'En Hilario Café, nos enorgullecemos de nuestra larga trayectoria con los más altos estándares de calidad. Con amplia experiencia en el rubro, nos hemos comprometido a brindar un ambiente ideal para que disfrutes de nuestra gastronomía con amigos y familiares.',
+  text: 'En Choi’s Café, nos enorgullecemos de nuestra larga trayectoria con los más altos estándares de calidad. Con amplia experiencia en el rubro, nos hemos comprometido a brindar un ambiente ideal para que disfrutes de nuestra gastronomía con amigos y familiares.',
 };
 
 export const metadataCompany = {
-  metadataBase: 'https://hilario-cafe.vercel.app/',
-  title: 'Hilario Café - Café de Especialidad',
+  metadataBase: 'https://chois-cafe.vercel.app/',
+  title: 'Choi’s Café - Café de Especialidad',
   description: 'Café de Especialidad, dedicación y pasión por lo que hacemos.',
 };
+
+export const sedes = [
+  {
+    id: 1,
+    title: 'Avellaneda',
+    adress: 'Av. Avellaneda 3780',
+    city: 'Flores',
+    tel: '1166136307',
+    whatsapp: '1166136307',
+    schedule:
+      'Horario: Lunes a Viernes 07:00 a 20:00hs | Sábados y Domingos 08:00 a 20:00hs',
+    appointment: 'https://maps.app.goo.gl/rQDHMRJeVE47QXJ9A',
+    image: 'sedes-1.webp',
+  },
+  {
+    id: 2,
+    title: 'Emilio Lamarca',
+    adress: 'Emilio Lamarca 698',
+    city: 'Flores',
+    tel: '1154823625',
+    whatsapp: '1154823625',
+    schedule:
+      'Horario: Lunes a Viernes 07:00 a 17:00hs | Sábados 08:00 a 13:00hs | Domingos 07:00 a 17:00hs',
+    appointment: 'https://maps.app.goo.gl/K15nicEoja6Mm5As8',
+    image: 'sedes-2.webp',
+  },
+];
 
 export const navigation = [
   {
--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -1,35 +1,16 @@
 'use client';
 
-import { company } from '@/app/constants/constants';
-import ClockIcon from '@/components/icons/ClockIcon';
-import FacebookIcon from '@/components/icons/FacebookIcon';
-import InstagramIcon from '@/components/icons/InstagramIcon';
+import { motion } from 'framer-motion';
+
+import { company, sedes } from '@/app/constants/constants';
+
+import Image from 'next/image';
 import LocationIcon from '@/components/icons/LocationIcon';
-import SocialIcons from '@/components/icons/SocialIcons';
 import WhatsappIcon from '@/components/icons/WhatsappIcon';
 
-import { motion } from 'framer-motion';
-import Image from 'next/image';
-
-interface Company {
-  id: string;
-  name: string;
-  adress?: string;
-  city?: string;
-  telephone?: string | null;
-  email?: string | null;
-  instagram?: string;
-  facebook?: string | null;
-  whatsapp?: string | null;
-  googlemaps?: string | null;
-  menu?: string | null;
-  openDays?: Array<{ day: string; hours: string[] }> | null;
-  footer: string;
-}
-
-const ContactoPage = () => {
+const ContactoSection = () => {
   return (
-    <section className='flex flex-col items-center'>
+    <section className='flex flex-col'>
       <section className='w-full max-w-[1920px] h-[180px] sm:h-[260px] md:h-[320px] lg:h-[400px] relative'>
         <div className='w-full h-full'>
           <Image
@@ -53,119 +34,96 @@ const ContactoPage = () => {
             </div>
           </div>
         </div>
-        <div className='absolute top-0 left-0 w-full h-full bg-gradient-to-r from-color-bg-secondary/70 md:from-color-bg-secondary/80 to-color-bg-secondary/70 md:to-color-bg-secondary/85'></div>
+        <div className='absolute top-0 left-0 w-full h-full bg-gradient-to-r from-color-bg-secondary/85 to-color-bg-secondary/85'></div>
       </section>
-      <section id='ubicacionSection' className='my-6 md:my-12 lg:my-16'>
-        <motion.div
-          initial={{ opacity: 0, y: 10 }}
-          whileInView={{
-            opacity: 1,
-            y: 0,
-            transition: {
-              duration: 0.3,
-              ease: 'easeOut',
-            },
-          }}
-          viewport={{ margin: '0px 0px -300px 0px', once: true }}
-          className='flex flex-col md:flex-row justify-center items-center gap-8 sm:gap-12 md:gap-10 lg:gap-16 mx-4 sm:mx-6 md:mx-8 lg:mx-10'
-        >
-          <article className='flex flex-col gap-5'>
-            <div>
-              <div className='flex items-center gap-2 mb-2'>
-                <span className=''>
-                  <ClockIcon className='w-7 h-7 lg:w-8 lg:h-8 fill-color-primary' />
-                </span>
-                <h5 className='text-xl md:text-2xl text-color-primary font-bold'>
-                  Horarios
-                </h5>
+      <section
+        id='sedesSection'
+        className='flex flex-col items-center my-8 md:my-14 lg:my-20 mx-4 sm:mx-6 md:mx-8 lg:mx-10'
+      >
+        <div className='grid grid-cols-1 sm:grid-cols-2 gap-x-5 md:gap-x-16 gap-y-20'>
+          {sedes.map((sede) => (
+            <motion.article
+              initial={{ opacity: 0, y: 10 }}
+              whileInView={{
+                opacity: 1,
+                y: 0,
+                transition: {
+                  duration: 0.3,
+                  ease: 'easeOut',
+                },
+              }}
+              viewport={{ margin: '0px 0px -300px 0px', once: true }}
+              key={sede.id}
+            >
+              <div className='w-[280px] md:w-[320px] lg:w-[405px] overflow-hidden rounded sm:rounded-lg'>
+                <a
+                  href={sede.appointment ? sede.appointment : undefined} // Cambiado para no enviar a ningún link si es null
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <Image
+                    className='hover:scale-105 transition-transform duration-700'
+                    src={`/assets/sedes/${sede.image}`}
+                    width={608}
+                    height={480}
+                    alt={`${sede.title} imagen`}
+                  />
+                </a>
               </div>
-              <ul className='flex flex-col gap-2'>
-                {company.openDays.map((openDay, idx) => (
-                  <li
-                    key={idx}
-                    className='flex gap-2 sm:text-lg lg:text-xl font-rubik-font font-medium text-center'
-                  >
-                    <span className='text-color-title font-semibold'>
-                      {openDay.day}
-                    </span>
-                    <div className='flex gap-3'>
-                      {openDay.hours.map((openHour, hourIdx) => (
-                        <span key={hourIdx}>{openHour}</span>
-                      ))}
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
-            {company.adress && company.city && (
               <div>
-                <div className='flex items-center gap-1 mb-2'>
-                  <span className=''>
-                    <LocationIcon className='w-6 h-6 lg:w-7 lg:h-7 fill-color-primary' />
+                {sede.title && (
+                  <h4 className='text-color-primary font-medium text-2xl mt-3'>
+                    {sede.title}
+                  </h4>
+                )}
+                {sede.adress && (
+                  <h6 className='text-color-text text-sm sm:text-base lg:text-lg'>
+                    {sede.adress}
+                  </h6>
+                )}
+                {sede.tel && (
+                  <span className='text-sm sm:text-base lg:text-lg text-color-primary'>
+                    {sede.tel}
                   </span>
-                  <h5 className='text-xl md:text-2xl text-color-primary font-bold'>
-                    Ubicación
-                  </h5>
+                )}
+                <p className='text-color-text text-sm sm:text-base lg:text-lg max-w-[280px] md:max-w-[320px] lg:max-w-[405px]'>
+                  {sede.schedule}
+                </p>
+                <div className='flex gap-3 mt-3'>
+                  {sede.appointment && (
+                    <div className='flex w-full'>
+                      <a
+                        href={sede.appointment}
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        className='flex justify-center items-center gap-1 bg-color-primary hover:bg-color-primary-dark transition-colors text-color-title-light text-sm sm:text-base md:text-lg  py-2 px-4 rounded w-full text-center'
+                      >
+                        <LocationIcon />
+                        <span>Ubicación</span>
+                      </a>
+                    </div>
+                  )}
+                  {sede.whatsapp && (
+                    <div className='flex w-full'>
+                      <a
+                        href={`https://api.whatsapp.com/send?phone=549${company.whatsapp}&text=Hola! Quería hacer una consulta`}
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        className='flex justify-center items-center gap-1 bg-[#25D366] hover:bg-[#05B146] transition-colors text-color-title-light text-sm sm:text-base md:text-lg  py-2 px-4 rounded w-full text-center'
+                      >
+                        <WhatsappIcon />
+                        <span>Whatsapp</span>
+                      </a>
+                    </div>
+                  )}
                 </div>
-                <span className='text-color-title sm:text-lg lg:text-xl font-semibold'>
-                  {company.adress}, {company.city}
-                </span>
               </div>
-            )}
-            <div>
-              <div className='flex items-center gap-2 mb-2'>
-                <span className=''>
-                  <SocialIcons className='w-6 h-6 lg:w-7 lg:h-7 fill-color-primary' />
-                </span>
-                <h5 className='text-xl md:text-2xl text-color-primary font-bold'>
-                  Redes
-                </h5>
-              </div>
-              <div className='flex gap-4 mt-3'>
-                {company.instagram && (
-                  <a
-                    href={`https://www.instagram.com/${company.instagram}/`}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                  >
-                    <InstagramIcon className='w-8 h-8 text-color-title hover:text-color-text transition-colors' />
-                  </a>
-                )}
-                {company.facebook && (
-                  <a
-                    href={`https://www.facebook.com/${company.facebook}/`}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                  >
-                    <FacebookIcon className='w-8 h-8 text-color-title hover:text-color-text transition-colors' />
-                  </a>
-                )}
-                {(company.whatsapp as Company['whatsapp']) && (
-                  <a
-                    href={`https://api.whatsapp.com/send?phone=549${company.whatsapp}&text=Hola! Quería hacer una consulta`}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                  >
-                    <WhatsappIcon className='w-8 h-8 text-color-title hover:text-color-text transition-colors' />
-                  </a>
-                )}
-              </div>
-            </div>
-          </article>
-          <article>
-            <iframe
-              className='w-72 h-56 sm:w-[450px] sm:h-[350px] md:w-[350px] lg:h-[400px] lg:w-[500px] rounded-md'
-              src={company.googlemaps}
-              width='500'
-              height='350'
-              loading='lazy'
-              referrerPolicy='no-referrer-when-downgrade'
-            ></iframe>
-          </article>
-        </motion.div>
+            </motion.article>
+          ))}
+        </div>
       </section>
     </section>
   );
 };
 
-export default ContactoPage;
+export default ContactoSection;
