5e7b961e359ed7d8d86a157c4c7b5d28592270b8
tobiajras <tobyajras@gmail.com>
2025-01-27 01:10:15 -0300

    mvp valparaiso-motors

--- src/app/constants/constants.tsx
+++ src/app/constants/constants.tsx
@@ -1,31 +1,32 @@
 export const company = {
   dark: true,
-  id: 'das-autos',
-  name: 'Das Autos',
-  adress: 'Dardo Rocha 856',
-  city: 'Berazategui',
-  email: null,
-  instagram: 'dasautosok',
-  facebook: 'Dasautos-Concesionaria',
-  whatsapp: ['1132060855'],
-  googlemapsLink: 'https://maps.app.goo.gl/8DiSkaj61PdCKNEk8',
+  id: 'valparaiso-motors',
+  name: 'Valparaiso Motors',
+  adress: 'Cdad. Valparaíso 4380',
+  city: 'Córdoba',
+  email: 'info@valparaisomotors.com.ar',
+  instagram: 'valparaiso.motors',
+  facebook: null,
+  whatsapp: ['3513092154'],
+  googlemapsLink: 'https://maps.app.goo.gl/yzieSibrVyXyLBQAA',
   googlemaps:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d209960.80203612667!2d-58.67087917187504!3d-34.68909656788392!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a32ed3c4dd9259%3A0x2b1dcd68c8db7ff0!2sDasAutos%20Concesionaria!5e0!3m2!1ses-419!2sar!4v1736441362832!5m2!1ses-419!2sar',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3403.220359644545!2d-64.1874756!3d-31.463123899999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9432a311b6662cbf%3A0x86142d22aba9828a!2sValparaiso%20Motors!5e0!3m2!1ses-419!2sar!4v1737660492990!5m2!1ses-419!2sar',
   openDays: [
-    { day: 'Lunes', hours: ['09:00 a 19:00'] },
-    { day: 'Martes', hours: ['09:00 a 19:00'] },
-    { day: 'Miércoles', hours: ['09:00 a 19:00'] },
-    { day: 'Jueves', hours: ['09:00 a 19:00'] },
-    { day: 'Viernes', hours: ['09:00 a 19:00'] },
-    // { day: 'Sábado', hours: ['09:00 a 19:00'] },
+    { day: 'Lunes', hours: ['09:30 a 13:30', '15:30 a 19:30'] },
+    { day: 'Martes', hours: ['09:30 a 13:30', '15:30 a 19:30'] },
+    { day: 'Miércoles', hours: ['09:30 a 13:30', '15:30 a 19:30'] },
+    { day: 'Jueves', hours: ['09:30 a 13:30', '15:30 a 19:30'] },
+    { day: 'Viernes', hours: ['09:30 a 13:30', '15:30 a 19:30'] },
+    { day: 'Sábado', hours: ['10:00 a 14:00'] },
+
     // { day: 'Domingo', hours: ['08:30 a 13:00', '17:00 a 21:00'] },
   ],
   footer: 'Compra y Venta de Autos, Seguridad y Confianza en Cada Kilómetro.',
 };
 
 export const metadataCompany = {
-  metadataBase: 'https://das-autos.vercel.app/',
-  title: 'Das Autos - Compra y Venta de Autos',
+  metadataBase: 'https://valparaiso-motors.vercel.app/',
+  title: 'Valparaiso Motors - Compra y Venta de Autos',
   description:
     'Compra y Venta de Autos, Seguridad y Confianza en Cada Kilómetro.',
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,9 +13,9 @@ const config: Config = {
         poppins: ['var(--font-poppins)'],
       },
       colors: {
-        'color-primary': '#F17C18',
-        'color-primary-dark': '#E96D02',
-        'color-primary-light': '#FFBC83',
+        'color-primary': '#CE1619',
+        'color-primary-dark': '#AD0608',
+        'color-primary-light': '#FF9293',
         'color-secondary': '#1E1E1E',
         'color-secondary-dark': '#222222',
         'color-secondary-light': '#E4E4E4',
