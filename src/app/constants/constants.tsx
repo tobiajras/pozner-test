6ac2ddcc0db19589cd3bbfa48c55f5a27168f9e2
tobiajras <tobyajras@gmail.com>
2025-02-04 11:42:10 -0300

    mvp gamma-automotores

--- src/app/constants/constants.tsx
+++ src/app/constants/constants.tsx
@@ -1,31 +1,31 @@
 export const company = {
   dark: true,
-  id: 'esposito-automotores',
-  name: 'Esposito Autos',
-  adress: 'Cervantes 3877',
-  city: 'Córdoba',
-  email: 'espositoautomototres@hotmail.com',
-  instagram: 'espositoautomotores',
-  facebook: 'espositoautomotores',
-  whatsapp: ['3564520688', '3564204912'],
-  googlemapsLink: 'https://maps.app.goo.gl/CvadVKDpbV8hPa1A9',
+  id: 'gamma-automotores',
+  name: 'Gamma Autos',
+  adress: 'Av. 14 3003',
+  city: 'Berazategui',
+  email: 'gammaautomotores@hotmail.com',
+  instagram: 'gammaautomotores',
+  facebook: null,
+  whatsapp: ['1150621895', '1137716646', '1166825777', '1120443064'],
+  googlemapsLink: 'https://maps.app.goo.gl/PW7sQGn8ZTsBvz336',
   googlemaps:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3404.5422427479134!2d-62.1083168!3d-31.426735800000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95cb2843646b4673%3A0x3971b74990d9e82b!2sEsposito%20Automotores!5e0!3m2!1ses-419!2sar!4v1737786413567!5m2!1ses-419!2sar',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13109.405742290395!2d-58.2285113!3d-34.7719271!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a32f2cb2b69675%3A0x8b1b6ea9e1ae82ff!2sGamma%20Automotores!5e0!3m2!1ses-419!2sar!4v1737787575472!5m2!1ses-419!2sar',
   openDays: [
     { day: 'Lunes', hours: ['09:00 a 19:00'] },
     { day: 'Martes', hours: ['09:00 a 19:00'] },
     { day: 'Miércoles', hours: ['09:00 a 19:00'] },
     { day: 'Jueves', hours: ['09:00 a 19:00'] },
     { day: 'Viernes', hours: ['09:00 a 19:00'] },
-    // { day: 'Sábado', hours: ['09:30 a 12:00'] },
+    { day: 'Sábado', hours: ['09:00 a 14:00'] },
     // { day: 'Domingo', hours: ['08:30 a 13:00', '17:00 a 21:00'] },
   ],
   footer: 'Compra y Venta de Autos, Seguridad y Confianza en Cada Kilómetro.',
 };
 
 export const metadataCompany = {
-  metadataBase: 'https://esposito-automotores.vercel.app/',
-  title: 'Esposito Automotores - Compra y Venta de Autos',
+  metadataBase: 'https://gamma-automotores.vercel.app/',
+  title: 'Gamma Automotores - Compra y Venta de Autos',
   description:
     'Compra y Venta de Autos, Seguridad y Confianza en Cada Kilómetro.',
 };
