fa55978742a63f4d1c59fd919a0c32311f3ab06a
tobiajras <tobyajras@gmail.com>
2025-02-19 08:52:27 -0300

    mvp ferrero-automotores

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,9 +13,9 @@ const config: Config = {
         poppins: ['var(--font-poppins)'],
       },
       colors: {
-        'color-primary': '#F6C604',
-        'color-primary-dark': '#DFB300',
-        'color-primary-light': '#FFE26B',
+        'color-primary': '#1A234C',
+        'color-primary-dark': '#091139',
+        'color-primary-light': '#A0ABDD',
         'color-secondary': '#1E1E1E',
         'color-secondary-dark': '#222222',
         'color-secondary-light': '#E4E4E4',
