45691952ddd2d2529d5973ed90928e9e3f8428ee
hostplacaresycocinas <hostplacaresycocinas@gmail.com>
2025-05-22 14:32:09 -0300

    layout header footer removed

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,5 +1,3 @@
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
 import Image from 'next/image';
 import { company, metadataCompany } from '../constants/constants';
 
