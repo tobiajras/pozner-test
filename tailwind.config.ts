933dc8599614a8e102309540595b84cb4f02ea5e
tobiajras <tobyajras@gmail.com>
2025-07-31 12:30:53 -0300

    mvp grautomotores

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,9 +13,9 @@ const config: Config = {
         manrope: ['var(--font-manrope)', 'sans-serif'],
       },
       colors: {
-        'color-primary': '#E80A0C',
-        'color-primary-dark': '#FD0002',
-        'color-primary-light': '#FF8383',
+        'color-primary': '#154688',
+        'color-primary-dark': '#054AA6',
+        'color-primary-light': '#81AAE1',
         'color-primary-admin': '#E9262C',
         'color-primary-dark-admin': '#F0161C',
         'color-secondary': '#1E1E1E',
